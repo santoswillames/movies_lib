57fc6ef2a9ce2715a257f9f1cf400cc010498330
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-12-19 17:19:31 -0300

    fetch api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-  return <div>Home</div>;
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const { results } = await res.json();
+
+    setTopMovies(results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${movieURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div>{topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}</div>
+  );
 };
 
 export default Home;
