fa128be112cb843d50b4eb880537a4a9018b1210
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-12-21 17:39:58 -0300

    update search component and movie component

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../../components/MovieCard";
+
+import "../styles/MovieGrid.css";
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
-  return <div>Search</div>;
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const { results } = await res.json();
+
+    setMovies(results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchUrl}?${apiKey}&query=${query}`;
+
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
+  return (
+    <div className="container">
+      <h2 className="title">
+        {" "}
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
